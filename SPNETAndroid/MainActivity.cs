e6b00cc0e30754b7a9fbfce39f74ba0d88da79ce
kana2022 <kkanat94@gmail.com>
2023-06-09 13:57:48 +0600

    1

--- SPNETAndroid/MainActivity.cs
+++ SPNETAndroid/MainActivity.cs
@@ -9,6 +9,19 @@ namespace SPNETAndroid
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            EditText phoneNumberText = FindViewById<EditText>(Resource.Id.phoneNumberText);
+            TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.translatedPhoneword);
+            Button translatedButton = FindViewById<Button>(Resource.Id.translateButton);
+
+            translatedButton.Click += (sender, e) =>
+            {
+                string translatedNumber = PhoneTranslator.ToNumber(phoneNumberText.Text);
+                if (string.IsNullOrWhiteSpace(translatedNumber))
+                    translatedPhoneWord.Text = string.Empty;
+                else
+                    translatedPhoneWord.Text = translatedNumber;
+            };
         }
     }
 }
